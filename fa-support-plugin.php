24577d91d9e0d74d12d780fc2edaed4278f5b234
vinogautam <neil@financialinsiders.ca>
2016-04-23 01:21:20 +0530

    Track surf page

--- fa-support-plugin.php
+++ fa-support-plugin.php
@@ -16,6 +16,18 @@
 		{
 			$this->save_lead();
 		}
+		
+		add_action( 'wp_footer', array( &$this, 'save_surfing_page'), 100 );
+	}
+	
+	function save_surfing_page()
+	{
+		$surfing_page = isset($_COOKIE['fa_surfing_page']) ? explode(",", $_COOKIE['fa_surfing_page']) : array();
+		//print_r(!in_array(get_the_title(), $surfing_page));
+		if(get_the_title() && !in_array(get_the_title(), $surfing_page))
+			$surfing_page[] = get_the_title();
+		//print_r($surfing_page);
+		setcookie("fa_surfing_page", implode(",", $surfing_page), time() + (86400 * 365), "/");
 	}
 	
 	function save_lead()
@@ -26,9 +38,22 @@
 		$lead_data['created'] = date("Y-m-d H:i:s");
 		//$lead_data['agent_id'] = '';
 		$lead_data['blog_id'] = get_current_blog_id();
+		$lead_data['form_url'] = $_SERVER['HTTP_REFERER'];
+		$lead_data['visited_page'] = $_COOKIE['fa_surfing_page'];
+		if(isset($_COOKIE['endorsement_track_link']) && !isset($_COOKIE['endorsement_tracked']))
+		{
+			$track_link = explode("#&$#", base64_decode(base64_decode($_COOKIE['endorsement_track_link'])));
+			if(count($track_link) == 3)
+			{
+				$get_results = $wpdb->get_row("select * from ".$wpdb->prefix . "endorsements where id=".$track_link[0]);
+				$lead_data['endorser_id'] = $get_results->endorser_id;
+			}
+		}
 		
 		$wpdb->insert($wpdb->prefix . "leads", $lead_data);
 		$lead_id = $wpdb->insert_id;
+		
+		
 	}
 	
 	function FA_install()
@@ -42,24 +67,25 @@
 			  id int(11) NOT NULL AUTO_INCREMENT,
 			   created datetime NOT NULL,
 			   first_name tinytext NOT NULL,
-			   last_name tinytext NOT NULL,
+			   last_name tinytext,
 			   email tinytext NOT NULL,
-			   city tinytext NOT NULL,
-			   phone tinytext NOT NULL,
-			   postal_code tinytext NOT NULL,
-			   province tinytext NOT NULL,
-			   dob tinytext NOT NULL,
-			   gender tinytext NOT NULL,
-			   occupation tinytext NOT NULL,
-			   income tinytext NOT NULL,
-			   retire_age tinytext NOT NULL,
-			   marital_status tinytext NOT NULL,
-			   retire_income tinytext NOT NULL,
-			   retirement_goal tinytext NOT NULL,
-			   own_business tinytext NOT NULL,
-			   comments text NOT NULL,
+			   city tinytext,
+			   phone tinytext,
+			   postal_code tinytext,
+			   province tinytext,
+			   dob tinytext,
+			   gender tinytext,
+			   occupation tinytext,
+			   income tinytext,
+			   retire_age tinytext,
+			   marital_status tinytext,
+			   retire_income tinytext,
+			   retirement_goal tinytext,
+			   own_business tinytext,
+			   comments text,
 			   agent_id text NOT NULL,
 			   blog_id text NOT NULL,
+			   newsetter boolean,
 			  PRIMARY KEY  (id) ) ENGINE=InnoDB";
 
 			require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
